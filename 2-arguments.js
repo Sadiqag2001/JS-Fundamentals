360a4997f2b2d055ca9ba605be15cddd1fc35613
Abubakar Ado Garba <abubakaradogarba@gmail.com>
2025-05-25 22:53:49 +0100

    initial commit

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+const args = process.argum.slice(2);
+if (argum.length === 0) {
+  console.log(No argument);
+} else if (argum.length === 1) {
+  console.log(Argument found);
+} else {
+  console.log(Arguments found);
+}
